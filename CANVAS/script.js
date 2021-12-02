914f3dd8f543e2c59b8b7729f03b7f8f46c40d61
willandru <willandru@hotmail.com>
2021-12-01 22:22:15 -0500

    Basic Drawing in a canvas

--- CANVAS/script.js
+++ CANVAS/script.js
@@ -1,6 +1,31 @@
+//GETTING THE CANVAS
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
+
+//console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height= window.innerHeight;
+
+//DRAWING A RECTANGLE
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height= window.innerHeight;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10,10,50,50);
+})
 
 ctx.fillStyle = 'white';
 ctx.fillRect(10,10,50,50);
+//DRAWING A CIRCLE
+
+
+
+ctx.fillStyle = 'red';
+
+ctx.strokeStyle='red';
+ctx.lineWidth=5;
+ctx.beginPath();
+ctx.arc(100,100,50,0, Math.PI*2);
+ctx.fill();
+ctx.stroke();
+
