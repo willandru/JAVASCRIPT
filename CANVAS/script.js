c17ea0093c3fbcd7a3bfe9d02be21d028370413f
willandru <willandru@hotmail.com>
2021-12-01 23:31:03 -0500

    Nice Lines drawing between particles

--- CANVAS/script.js
+++ CANVAS/script.js
@@ -95,10 +95,10 @@ function init(){
 
 
 function animate(){
-    //ctx.clearRect(0,0 ,canvas.width, canvas.height);
+    ctx.clearRect(0,0 ,canvas.width, canvas.height);
     //draw();
-    ctx.fillStyle = 'rgba(0,0,0,0.02)';
-    ctx.fillRect(0,0, canvas.width, canvas.height);
+   /*  ctx.fillStyle = 'rgba(0,0,0,0.02)';
+    ctx.fillRect(0,0, canvas.width, canvas.height); */
 
     handleParticles();
     requestAnimationFrame(animate);
@@ -112,12 +112,25 @@ function handleParticles(){
         particlesArray[i].update();
         particlesArray[i].draw();
 
-        if(particlesArray[i].size <= 0.3){
-            particlesArray.splice(i,1);
-            i--;
-           // console.log(particlesArray.length)
-        }
+            for(let j=i; j<particlesArray.length ;j++){
+                const dx = particlesArray[i].x - particlesArray[j].x;
+                const dy = particlesArray[i].y - particlesArray[j].y;
 
+                const distance = Math.sqrt(dx*dx + dy*dy);
+                if(distance <100){
+                    ctx.beginPath();
+                    ctx.strokeStyle= particlesArray[i].color;
+                    ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                    ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                    ctx.stroke();
+                }
+            }
+
+            if(particlesArray[i].size <= 0.3){
+                particlesArray.splice(i,1);
+                i--;
+               // console.log(particlesArray.length)
+            }
     }
 }
 
