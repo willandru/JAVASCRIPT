9a5dde5c8e486b709a68219d4eef385b1fe0e2bf
willandru <willandru@hotmail.com>
2021-12-01 22:53:04 -0500

    NICE ANIMATION: Particles desapearing from screen

--- CANVAS/script.js
+++ CANVAS/script.js
@@ -1,3 +1,5 @@
+const particlesArray =[];
+
 //GETTING THE CANVAS
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
@@ -7,12 +9,12 @@ canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
 
 //DRAWING A RECTANGLE
-window.addEventListener('resize', function(){
+/* window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height= window.innerHeight;
     ctx.fillStyle = 'white';
     ctx.fillRect(10,10,50,50);
-})
+}) */
 
 ctx.fillStyle = 'white';
 ctx.fillRect(10,10,50,50);
@@ -20,24 +22,84 @@ ctx.fillRect(10,10,50,50);
 
 
 const mouse ={
-    x: null,
-    y: null,
+    x: undefined,
+    y: undefined,
 }
 
 canvas.addEventListener('click', function(event){
     mouse.x= event.x;
     mouse.y= event.y;
-    drawCircle();
+    
+});
+
+canvas.addEventListener('mousemove', function(event){
+    mouse.x= event.x;
+    mouse.y= event.y;
+    
 });
 
 
-function drawCircle(){
-    ctx.fillStyle = 'red';
+/* function drawCircle(){
+    ctx.fillStyle = 'green';
     ctx.beginPath();
     ctx.arc(mouse.x,mouse.y,50,0, Math.PI*2);
     ctx.fill();
 
+} */
+
+class Particle{
+    constructor(){
+        /* this.x= mouse.x;
+        this.y= mouse.y; */
+
+        this.x= Math.random()*canvas.width;
+        this.y= Math.random()*canvas.height;
+
+
+        this.size= Math.random()*5+1;
+        this.speedX= Math.random()*3-1.5;
+        this.speedY= Math.random()*3-1.5;
+    }
+
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,50,0, Math.PI*2);
+        ctx.fill();
+    }
 }
+
+function init(){
+    for(let i=0; i<100; i++){
+        particlesArray.push(new Particle());
+    }
+}
+
+
+function animate(){
+    ctx.clearRect(0,0 ,canvas.width, canvas.height);
+    //draw();
+    handleParticles();
+    requestAnimationFrame(animate);
+}
+init();
+
+
+function handleParticles(){
+    for(let i=0; i<particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+}
+
+
+animate();
+
 
 
 /* ctx.fillStyle = 'red';
